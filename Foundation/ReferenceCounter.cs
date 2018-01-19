e9603f2dca12697e455cbb69ffe0eb57932bff37
csaba.bernath <csaba.bernath@gmail.com>
2018-01-19 17:05:41 +0100

    Reference counting

---
+++ Foundation/ReferenceCounter.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+
+namespace Foundation
+{
+    public sealed class ReferenceCounter
+    {
+        private int _value;
+
+        public void Add()
+        {
+            Interlocked.Increment(ref _value);
+        }
+
+        public void Remove()
+        {
+            Interlocked.Decrement(ref _value);
+        }
+
+        public int Value => _value;
+    }
+}
