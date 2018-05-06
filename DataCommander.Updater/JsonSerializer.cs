b5dbce00e22f5778be352bc0bad7afe8fc545315
Csaba Bernáth <csaba.bernath@gmail.com>
2018-05-06 08:51:16 +0200

    Moving ISerializer to Core

--- DataCommander.Updater/JsonSerializer.cs
+++ DataCommander.Updater/JsonSerializer.cs
@@ -6,9 +6,7 @@ namespace DataCommander.Updater
     public sealed class JsonSerializer : ISerializer
     {
         public string Serialize(object objectGraph) =>
-            JsonConvert.SerializeObject(
-                objectGraph,
-                new JsonSerializerSettings {TypeNameHandling = TypeNameHandling.Objects});
+            JsonConvert.SerializeObject(objectGraph, new JsonSerializerSettings {TypeNameHandling = TypeNameHandling.Objects});
 
         public T Deserialize<T>(string serializedObjectGraph) =>
             JsonConvert.DeserializeObject<T>(serializedObjectGraph, new JsonSerializerSettings {TypeNameHandling = TypeNameHandling.Objects});
