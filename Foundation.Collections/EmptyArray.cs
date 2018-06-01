6edb17bcc1d7aea840d2f5446ac6de8be6315bec
Csaba Bernáth <csaba.bernath@gmail.com>
2018-06-01 07:06:59 +0200

    Adding Empty collections

--- Foundation.Collections/EmptyArray.cs
+++ Foundation.Collections/EmptyArray.cs
@@ -1,11 +1,7 @@
-﻿#if FOUNDATION_4_6 || FOUNDATION_4_7
-
-namespace Foundation.Collections
+﻿namespace Foundation.Collections
 {
     internal static class EmptyArray<T>
     {
         public static readonly T[] Value = new T[0];
     }
 }
-
-#endif
---
+++ Foundation.Collections/EmptyReadOnlyCollection.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Foundation.Collections
+{
+    public static class EmptyReadOnlyCollection<T>
+    {
+        public static readonly ReadOnlyCollection<T> Value = new ReadOnlyCollection<T>(EmptyArray<T>.Value);
+    }
+}
