61e946f38d9fe1ec0757b25e56ac0cf093b6605c
Csaba Bernáth <csaba.bernath@gmail.com>
2018-11-26 12:51:48 +0100

    SqlStatementExtensions

--- Foundation/.NetStandard-2.0/Data/SqlClient/SqlStatementExtensions.cs
+++ Foundation/.NetStandard-2.0/Data/SqlClient/SqlStatementExtensions.cs
@@ -4,151 +4,54 @@ using System.Globalization;
 
 namespace Foundation.Data.SqlClient
 {
-    /// <summary>
-    /// 
-    /// </summary>
     public static class SqlStatementExtensions
     {
-        /// <summary>
-        /// 
-        /// </summary>
         private const byte True = 1;
-
-        /// <summary>
-        /// 
-        /// </summary>
         private const byte False = 0;
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="source"></param>
-        /// <returns></returns>
-        public static byte ToTSqlBit(this bool source)
-        {
-            return source ? True : False;
-        }
+        public static byte ToTSqlBit(this bool source) => source ? True : False;
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="source"></param>
-        /// <returns></returns>
         public static string ToTSqlDateTime(this DateTime source)
         {
             var timeOfDay = source.TimeOfDay;
             string format;
 
-            if (timeOfDay.TotalMilliseconds == 0)
-            {
+            if (timeOfDay == TimeSpan.Zero)
                 format = "yyyyMMdd";
-            }
             else
-            {
-                format = source.Millisecond == 0 ? "yyyyMMdd HH:mm:ss" : "yyyyMMdd HH:mm:ss.fff";
-            }
+                format = source.Millisecond == 0
+                    ? "yyyyMMdd HH:mm:ss"
+                    : "yyyyMMdd HH:mm:ss.fff";
 
             return $"'{source.ToString(format, CultureInfo.InvariantCulture)}'";
         }
 
-        /// <summary>
-        /// Converts a <see cref="System.Decimal"/> value to Microsoft SQL Server decimal string.
-        /// </summary>
-        /// <param name="source"></param>
-        /// <returns></returns>
-        public static string ToTSqlDecimal(this decimal source)
-        {
-            return source.ToString(NumberFormatInfo.InvariantInfo);
-        }
+        public static string ToTSqlDecimal(this decimal source) => source.ToString(NumberFormatInfo.InvariantInfo);
+        public static string ToTSqlInt(this int? source) => source != null ? source.ToString() : SqlNull.NullString;
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="source"></param>
-        /// <returns></returns>
-        public static string ToTSqlInt(this int? source)
-        {
-            return source != null ? source.ToString() : SqlNull.NullString;
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="source"></param>
-        /// <returns></returns>
         public static string ToTSqlVarChar(this string source)
         {
             string target;
             if (source != null)
-            {
                 target = "'" + source.Replace("'", "''") + "'";
-            }
             else
-            {
                 target = SqlNull.NullString;
-            }
+
             return target;
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="source"></param>
-        /// <returns></returns>
         public static string ToTSqlNVarChar(this string source)
         {
             string target;
             if (source != null)
-            {
                 target = "N'" + source.Replace("'", "''") + "'";
-            }
             else
-            {
                 target = SqlNull.NullString;
-            }
+
             return target;
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="value"></param>
-        /// <returns></returns>
-        public static SqlDateTime ToSqlDateTime(this DateTime? value)
-        {
-            SqlDateTime returnValue;
-
-            if (value != null)
-            {
-                returnValue = value.Value;
-            }
-            else
-            {
-                returnValue = SqlDateTime.Null;
-            }
-
-            return returnValue;
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="value"></param>
-        /// <returns></returns>
-        public static SqlInt32 ToSqlInt32(this int? value)
-        {
-            SqlInt32 returnValue;
-
-            if (value != null)
-            {
-                returnValue = value.Value;
-            }
-            else
-            {
-                returnValue = SqlInt32.Null;
-            }
-
-            return returnValue;
-        }
+        public static SqlDateTime ToSqlDateTime(this DateTime? value) => value ?? SqlDateTime.Null;
+        public static SqlInt32 ToSqlInt32(this int? value) => value ?? SqlInt32.Null;
     }
 }
