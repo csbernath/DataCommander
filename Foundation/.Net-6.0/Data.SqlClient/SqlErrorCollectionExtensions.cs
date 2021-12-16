f4ab17b827e8908d078306626de86e069ef89b7f
Csaba Bernáth <csaba.bernath@gmail.com>
2021-12-16 08:12:12 +0100

    SqlClient

--- Foundation/.Net-6.0/Data.SqlClient/SqlErrorCollectionExtensions.cs
+++ Foundation/.Net-6.0/Data.SqlClient/SqlErrorCollectionExtensions.cs
@@ -1,27 +1,26 @@
-﻿// using System.Text;
-// using Foundation.Data.SqlClient;
-// using Microsoft.Data.SqlClient;
-//
-// namespace Foundation.Data.SqlClient2;
-//
-// public static class SqlErrorCollectionExtensions
-// {
-//     public static string ToLogString(this SqlErrorCollection errors)
-//     {
-//         string message = null;
-//
-//         if (errors != null)
-//         {
-//             var stringBuilder = new StringBuilder();
-//             foreach (SqlError error in errors)
-//             {
-//                 var s = error.ToLogString();
-//                 stringBuilder.AppendLine(s);
-//             }
-//
-//             message = stringBuilder.ToString();
-//         }
-//
-//         return message;
-//     }
-// }
+﻿using System.Text;
+using Microsoft.Data.SqlClient;
+
+namespace Foundation.Data.SqlClient;
+
+public static class SqlErrorCollectionExtensions
+{
+    public static string ToLogString(this SqlErrorCollection errors)
+    {
+        string message = null;
+
+        if (errors != null)
+        {
+            var stringBuilder = new StringBuilder();
+            foreach (SqlError error in errors)
+            {
+                var s = error.ToLogString();
+                stringBuilder.AppendLine(s);
+            }
+
+            message = stringBuilder.ToString();
+        }
+
+        return message;
+    }
+}
