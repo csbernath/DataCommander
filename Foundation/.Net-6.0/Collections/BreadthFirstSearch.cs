8458cca36da285245cc5db8688108b6d829c4d01
Csaba Bernáth <csaba.bernath@gmail.com>
2022-03-22 07:18:21 +0100

    BreadthFirstSearch

---
+++ Foundation/.Net-6.0/Collections/BreadthFirstSearch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Foundation.Assertions;
+
+namespace Foundation.Collections;
+
+public static class BreadthFirstSearch
+{
+    public static void Search<TNode>(TNode node, Func<TNode, IEnumerable<TNode>> getChildNodes, Func<TNode, bool> visit)
+    {
+        Assert.IsNotNull(getChildNodes);
+        Assert.IsNotNull(visit);
+
+        var queue = new Queue<TNode>();
+        queue.Enqueue(node);
+
+        while (queue.Count > 0)
+        {
+            var currentNode = queue.Dequeue();
+            var continueSearch = visit(currentNode);
+
+            if (!continueSearch)
+                break;
+
+            var childNodes = getChildNodes(currentNode);
+
+            foreach (var childNode in childNodes)
+                queue.Enqueue(childNode);
+        }
+    }
+}
