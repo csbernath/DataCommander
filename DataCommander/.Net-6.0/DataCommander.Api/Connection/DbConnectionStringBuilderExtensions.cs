dd256bfdf0bd6877cc26f70ea8c14d0fca2e6296
Csaba Bernáth <csaba.bernath@gmail.com>
2022-06-23 12:31:34 +0200

    Applying nullability changes

--- DataCommander/.Net-6.0/DataCommander.Api/Connection/DbConnectionStringBuilderExtensions.cs
+++ DataCommander/.Net-6.0/DataCommander.Api/Connection/DbConnectionStringBuilderExtensions.cs
@@ -4,10 +4,10 @@ namespace DataCommander.Api.Connection;
 
 internal static class DbConnectionStringBuilderExtensions
 {
-    public static string GetValue(this DbConnectionStringBuilder dbConnectionStringBuilder, string keyword)
+    public static string? GetValue(this DbConnectionStringBuilder dbConnectionStringBuilder, string keyword)
     {
         var contains = dbConnectionStringBuilder.TryGetValue(keyword, out var valueObject);
-        var value = contains ? (string) valueObject : null;
+        var value = contains ? (string?)valueObject : null;
         return value;
     }
 }
