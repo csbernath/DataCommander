537245c3ef0ad86eb11649a6c04ca7d4e9ebcd54
Csaba Bernáth <csaba.bernath@gmail.com>
2023-03-27 14:49:01 +0200

    Updating assembly info from 2022 to 2023

--- DataCommander/.Net-7.0/Providers/DataCommander.Providers.SqlServerCe40/Properties/AssemblyInfo.cs
+++ DataCommander/.Net-7.0/Providers/DataCommander.Providers.SqlServerCe40/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 //[assembly: AssemblyTitle("DataCommander.Application.SqlServerCe40")]
 //[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyCopyright("Copyright © 2002-2022 Csaba Bernáth")]
+[assembly: AssemblyCopyright("Copyright © 2002-2023 Csaba Bernáth")]
 //[assembly: AssemblyCompany("Csaba Bernáth")]
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
