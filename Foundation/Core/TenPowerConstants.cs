a02f004f2bc213e0a7951a1ab8d4c44cf24f9155
Csaba Bernáth <csaba.bernath@gmail.com>
2025-04-30 09:17:55 +0200

    TenPowerConstants expanded

--- Foundation/Core/TenPowerConstants.cs
+++ Foundation/Core/TenPowerConstants.cs
@@ -4,5 +4,8 @@ public static class TenPowerConstants
 {
     public const short TenPower3 = 1000;
     public const int TenPower6 = TenPower3 * TenPower3;
-    public const int TenPower9 = TenPower6 * TenPower3;
+    public const int TenPower9 = TenPower3 * TenPower6;
+    public const long TenPower12 = (long)TenPower3 * TenPower9;
+    public const long TenPower15 = TenPower3 * TenPower12;
+    public const long TenPower18 = TenPower3 * TenPower15;
 }
