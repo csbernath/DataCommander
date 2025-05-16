8ae1cc5b43ca8b8d8551ceac0a16c52cf3c81f08
Csaba Bernáth <csaba.bernath@gmail.com>
2025-05-16 13:37:36 +0200

    Experimental json repo save for connections

--- DataCommander/DataCommander.Application/Connection/ConnectionInfoRepository.cs
+++ DataCommander/DataCommander.Application/Connection/ConnectionInfoRepository.cs
@@ -2,9 +2,12 @@
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
 using Foundation.Configuration;
 using Foundation.Log;
 using Newtonsoft.Json;
+using JsonSerializer = Newtonsoft.Json.JsonSerializer;
 
 namespace DataCommander.Application.Connection;
 
@@ -35,6 +38,27 @@ public static class ConnectionInfoRepository
     }
 
     public static void Save(IEnumerable<ConnectionInfo> connectionInfos)
+    {
+        var connectionInfoArray = connectionInfos.ToArray();
+        
+        SaveWithNewtonsoft(connectionInfoArray);
+        
+        var connectionDtos = connectionInfoArray
+            .Select(connectionProperties => connectionProperties.ToConnectionDto());
+        var path = GetPath();
+        using var fileStream = new FileStream(path + "New", FileMode.Create, FileAccess.Write, FileShare.None);
+        fileStream.Write(Encoding.UTF8.Preamble);
+        
+        var utf8JsonWriter = new Utf8JsonWriter(fileStream, new JsonWriterOptions { Indented = true });
+        System.Text.Json.JsonSerializer.Serialize(utf8JsonWriter, connectionDtos, typeof(IEnumerable<ConnectionDto>), new JsonSerializerOptions
+        {
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+            IncludeFields = true
+        });
+        utf8JsonWriter.Dispose();
+    }
+
+    private static void SaveWithNewtonsoft(IEnumerable<ConnectionInfo> connectionInfos)
     {
         var connectionDtos = connectionInfos
             .Select(connectionProperties => connectionProperties.ToConnectionDto());
