9f53ada0f8d298b8c8c6c9b7bcf7c16be47e19ec
Csaba Bernáth <csaba.bernath@gmail.com>
2025-09-10 08:27:50 +0200

    Visual Studio 2026 Insiders added to credits

--- DataCommander/DataCommander.Application/AboutForm.cs
+++ DataCommander/DataCommander.Application/AboutForm.cs
@@ -51,7 +51,8 @@ Including <a href=""https://github.com/csbernath/DataCommander/blob/master/Found
 Credits:
 <ul style=""list-style-type:none"">
     <li><a href=""https://www.jetbrains.com/rider/"">JetBrains Rider</a></li>
-    <li><a href=""https://www.visualstudio.com/vs/community/"">Visual Studio Community</a></li>
+    <li><a href=""https://www.visualstudio.com/vs/community/"">Visual Studio 2022 Community</a></li>
+    <li><a href=""https://visualstudio.microsoft.com/insiders/"">Visual Studio 2026 Insiders</a></li>
     <li><a href=""https://www.jetbrains.com/resharper/"">JetBrains R# ReSharper</a></li>
     <li><a href=""https://github.com/JanKallman/EPPlus"">EPPlus Excel generator</a></li>
     <li><a href=""https://learn.microsoft.com/en-us/dotnet/standard/data/sqlite/?tabs=net-cli"">Microsoft SQLite provider</a></li>
