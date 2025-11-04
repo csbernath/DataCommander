c2a001b167cba5c865edba5d78580dcee6c281ca
Csaba Bernáth <csaba.bernath@gmail.com>
2025-11-04 08:34:51 +0100

    SqlServerVersionInfoRepository expanded

--- DataCommander/Providers/DataCommander.Providers.SqlServer/SqlServerVersionInfoRepository.cs
+++ DataCommander/Providers/DataCommander.Providers.SqlServer/SqlServerVersionInfoRepository.cs
@@ -61,5 +61,6 @@ internal static class SqlServerVersionInfoRepository
         new("16.00.1135", "5046861 Security update for SQL Server 2022 GDR: November 12, 2024 "),
         new("16.00.1150", "5065221 Security update for SQL Server 2022 GDR: September 9, 2025"),
         new("16.00.4100", "5033592 Security update for SQL Server 2022 CU10: January 9, 2024"),
+        new("17.00.0925", "Microsoft SQL Server 2025 Release Candidate 1 (RC 1), 2025-09-16")
     }.ToDictionary(i => i.Version);
 }
